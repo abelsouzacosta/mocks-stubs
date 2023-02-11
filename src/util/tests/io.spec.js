cd5c0b45dfb8ff9aa6b01a2710c32ad7215d3d24
Abel Junior <abel.costa@webjump.com.br>
2023-02-11 10:46:36 -0300

    [chore]: mocking external dependencies

--- src/util/tests/io.spec.js
+++ src/util/tests/io.spec.js
@@ -3,12 +3,22 @@ import { writeData } from '../io';
 import { promises as fs } from 'fs';
 
 vi.mock('fs');
+vi.mock('path', () => {
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length - 1];
+      }
+    }
+  }
+});
 
 describe("writeData", () => {
   it("should create a file with the name provided in the filesystem containing the content", () => {
     const content = 'WriteData';
     const filename = 'text.txt';
-    writeData(content, filename);
-    expect(fs.writeFile).toBeCalled();
+    const result = writeData(content, filename);
+    expect(result).resolves.toBeUndefined();
+    expect(fs.writeFile).toBeCalledWith(filename, content);
   });
 });
